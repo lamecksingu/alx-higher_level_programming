65f2feedb1fb3fc56069e42ef95d2c9d7874086d
lamecksingu <lamecksingu9619@gmailo.com>
2022-06-09 12:56:11 +0000

    Update dictionary

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """Replace or add key or value pairs in a dictionary given"""
+
+    a_dictionary[key] = value
+    return (a_dictionary)
