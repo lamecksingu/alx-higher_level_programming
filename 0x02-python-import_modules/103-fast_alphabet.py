b1d10ccd7882be94fd4717881af3da6543d36f96
lamecksingu <singulameck9619@gmail.com>
2023-11-30 22:50:07 +0300

    Some modification

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,2 +1,2 @@
 #!/usr/bin/python3
-__import__('os').write(1, (__import__('string').ascii_uppercase + '\n').encode())
+__import__('os').write(1, (__import__('string').ascii_uppercase + '\n').encode('utf-8'))
