6ae4fb5441942a7813f7dd88ae6ca0673386d3d7
lamecksingu <singulameck9619@gmail.com>
2023-11-30 22:36:18 +0300

    Magic calculations

---
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    c = 0
+
+    if a < b:
+        from magic_calculation_102 import add
+        from magic_calculation_102 import sub
+
+        c = add(a, b)
+
+        for i in range(4, 6):
+            c = add(c, i)
+
+        return c
+    else:
+        return sub(a, b)
