a2ce00b6f3f7896288bc2c70baae3b505c5eadff
lamecksingu <singulameck9619@gmail.com>
2023-12-01 11:08:41 +0300

    Some other changes

--- 0x02-python-import_modules/103-fast_alphabet.py
+++ 0x02-python-import_modules/103-fast_alphabet.py
@@ -1,3 +1,2 @@
 #!/usr/bin/python3
-import string
-__import__('os').write(1, getattr(__import__(string.__name__),'ascii_uppercase').encode() + b'\n')
+__import__("os").system('echo {}'.format(''.join(__import__("string").ascii_uppercase)))
