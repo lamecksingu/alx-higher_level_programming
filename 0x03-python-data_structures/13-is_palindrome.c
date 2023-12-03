107675c2172d8f3f6d99e03749bbed24959e6267
lamecksingu <singulameck9619@gmail.com>
2023-12-03 23:21:32 +0300

    A header file and a function to check for palindromity in a linked list

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,48 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * is_palindrome - a function to check for palindromity
+ * @head: head of the linked list
+ * Return: 1 if palindrome found 0 otherwise
+ */
+int is_palindrome(listint_t **head)
+{
+	listint_t *slow = *head;
+	listint_t *fast = *head;
+	listint_t *prev = NULL;
+
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return (1);
+	}
+
+	/*Finding the middle of the list and reversing the first half*/
+	while (fast != NULL && fast->next != NULL)
+	{
+		fast = fast->next->next;
+		listint_t *temp = slow->next;
+
+		slow->next = prev;
+		prev = slow;
+		slow = temp;
+	}
+
+	/*Adjusting for odd length*/
+	if (fast != NULL)
+	{
+		slow = slow->next;
+	}
+
+	/*Comparing both halves for palindrome*/
+	while (prev != NULL && slow != NULL)
+	{
+		if (prev->n != slow->n)
+		{
+			return (0);
+		}
+		prev = prev->next;
+		slow = slow->next;
+	}
+	return (1);
+}
