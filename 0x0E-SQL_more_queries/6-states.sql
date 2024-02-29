103af31a18ab64fedc93e24fada08e599bd9460c
lamecksingu <singulameck9619@gmail.com>
2024-02-29 16:26:48 +0300

    A script that creates database hbtn_0d_usa and table states

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,3 @@
+-- A script to create database hbtn_0d_usa and table states
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states(`id` INT UNIQUE AUTO_INCREMENT PRIMARY KEY, `name` VARCHAR(256) NOT NULL);
