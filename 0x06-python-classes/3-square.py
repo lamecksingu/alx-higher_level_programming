96d7ec55e96dcd8ec258774bcf134aaa19d2bef4
lamecksingu <singulameck9619@gmail.com>
2024-03-20 16:26:47 +0300

    Error in pycodestyle validation

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -5,7 +5,7 @@
 class Square:
     """ Square class"""
     def __init__(self, size=0):
-        if type(size) != int:
+        if type(size) is not int:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
